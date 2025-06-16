7f46a295bcfa30e9c3212df9fb3482ff08fad445
surexnx <112638211+nukakmetro@users.noreply.github.com>
2025-06-16 23:02:16 +0300

    added example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Айдар Тукаев on 16.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sources/ContextMenu/Core/ContextMenuSupportable.swift
@@ -0,0 +1,72 @@
+//
+//  ContextMenuSupportable.swift
+//  BootcampContextMenus
+//
+//  Created by Nikita Konashenko on 03.11.2024.
+//
+
+import UIKit
+
+public protocol ContextMenuSupportable {
+    var contextMenuConfiguration: ContextMenuConfiguration? { get }
+    var previewForHighlightingContextMenu: UITargetedPreview? { get }
+    var previewForDismissingContextMenu: UITargetedPreview? { get }
+}
+
+public extension ContextMenuSupportable {
+    var previewForHighlightingContextMenu: UITargetedPreview? { nil }
+    var previewForDismissingContextMenu: UITargetedPreview? { nil }
+}
+
+/// Convenience wrapper for UIContextMenuConfiguration.
+public struct ContextMenuConfiguration {
+    public typealias ContextMenuContentPreviewTransform = (UIViewController?) -> Void
+    public typealias ContextMenuActionTransform = (UIMenu?) -> Void
+
+    public let previewProvider: UIContextMenuContentPreviewProvider?
+    public let actionProvider: UIContextMenuActionProvider?
+
+    public init(
+        previewProvider: UIContextMenuContentPreviewProvider? = nil,
+        actionProvider: UIContextMenuActionProvider? = nil
+    ) {
+        self.previewProvider = previewProvider
+        self.actionProvider = actionProvider
+    }
+
+    public func make(
+        with identifier: NSCopying? = nil,
+        previewTransform: ContextMenuContentPreviewTransform? = nil,
+        actionTransform: ContextMenuActionTransform? = nil
+    ) -> UIContextMenuConfiguration {
+        return .init(
+            identifier: identifier,
+            previewProvider: previewProvider.flatMap { previewProvider in
+                return {
+                    let preview = previewProvider()
+                    previewTransform?(preview)
+                    return preview
+                }
+            },
+            actionProvider: actionProvider.flatMap { actionProvider in
+                return { suggestedActions in
+                    let action = actionProvider(suggestedActions)
+                    actionTransform?(action)
+                    return action
+                }
+            }
+        )
+    }
+}
+
+// MARK: - Helpers
+
+public extension UICollectionView {
+    func contextMenuSupportableCell(for identifier: NSCopying) -> ContextMenuSupportable? {
+        return contextMenuSupportableCell(for: identifier as? IndexPath)
+    }
+
+    func contextMenuSupportableCell(for indexPath: IndexPath?) -> ContextMenuSupportable? {
+        return indexPath.flatMap { indexPath in cellForItem(at: indexPath) as? ContextMenuSupportable }
+    }
+}
